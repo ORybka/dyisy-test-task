3650fcdb67650e36d33d4a070f9b42f3c29db647
Olha Rybchynska <olha.rybchynska@gmail.com>
2020-12-17 13:51:29 +0200

    add flex layout

---
+++ main.js
@@ -0,0 +1,57 @@
+const game = [
+  {
+    id: 0,
+    name: 'baccarat',
+    label: '',
+    description: 'Welcome Bonus',
+    price: 250,
+    bonus: ['+ 20 Free Spins'],
+    userNum: 1816,
+    rating: 9.3,
+    stars: 4.5
+  },
+  {
+    id: 1,
+    name: 'black-jack',
+    label: 'trending',
+    description: '200% Up to',
+    price: 500,
+    bonus: ['+ 20 FREE SPINS'],
+    userNum: 2063,
+    rating: 9.9,
+    stars: 4.5
+  },
+  {
+    id: 2,
+    name: 'pai-gow',
+    label: 'exclusive',
+    description: '100% Up to',
+    price: 200,
+    bonus: ['+ 25 Free Spins on Book of Dead'],
+    userNum: 1890,
+    rating: 9.4,
+    stars: 4.5
+  },
+  {
+    id: 3,
+    name: 'poker',
+    label: 'player favourite',
+    description: '100% Up to',
+    price: 1500,
+    bonus: ['+ 150 Zee Spins', '+ 500 Zee Points'],
+    userNum: 2589,
+    rating: 10,
+    stars: 4.5
+  },
+  {
+    id: 4,
+    name: 'roulette',
+    label: 'number 1 in europe',
+    description: '200% Up to',
+    price: 3000,
+    bonus: ['+ 30 FREE SPINS'],
+    userNum: 1975,
+    rating: 9.8,
+    stars: 4.5
+  }
+]
