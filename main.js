32c7c76443926ba11e63e57c51fde71b28b19fbf
Olha Rybchynska <olha.rybchynska@gmail.com>
2020-12-17 22:03:29 +0200

    add completed desktop vers

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const game = [
     bonus: ['+ 20 Free Spins'],
     userNum: 1816,
     rating: 9.3,
-    stars: 5
+    stars: 3.5
   },
   {
     id: 1,
@@ -23,7 +23,7 @@ const game = [
     bonus: ['+ 20 FREE SPINS'],
     userNum: 2063,
     rating: 9.9,
-    stars: 5
+    stars: 4.5
   },
   {
     id: 2,
@@ -34,7 +34,7 @@ const game = [
     bonus: ['+ 25 Free Spins on Book of Dead'],
     userNum: 1890,
     rating: 9.4,
-    stars: 5
+    stars: 4.5
   },
   {
     id: 3,
@@ -45,7 +45,7 @@ const game = [
     bonus: ['+ 150 Zee Spins', '+ 500 Zee Points'],
     userNum: 2589,
     rating: 10,
-    stars: 5
+    stars: 4.5
   },
   {
     id: 4,
@@ -56,18 +56,19 @@ const game = [
     bonus: ['+ 30 FREE SPINS'],
     userNum: 1975,
     rating: 9.8,
-    stars: 5
+    stars: 4.5
   }
 ]
 
 const gameContainer = document.querySelector('.game-container');
 
 function createGame() {
+  game.sort((a, b) => (a.rating < b.rating) ? 1 : -1)
   game.forEach((el) => {
     addGame(el);
     addImage(el);
     addDescription(el);
-    addStars(el);
+    addStars(el, el.stars);
     addGameRating(el);
     addBtn();
   });
@@ -83,32 +84,49 @@ function addGame({ id }) {
 function addImage({ name, label }) {
   imageElement = document.createElement('div');
   imageElement.className = 'image-container';
-  imageElement.innerHTML = `
-  <img src="assets/${name}.jpg"><span class="game-label">${label}</span>`;
+  imageElement.innerHTML = `<span class="game-label">${label}</span><div class="image" style="background-image: url(&quot;assets/${name}.jpg&quot;);"></div>`;
+  if(label === '') {
+    imageElement.removeChild(imageElement.firstChild);
+  }
   gameElement.appendChild(imageElement);
 };
 
 function addDescription({ description, price, bonus}) {
+  bonus = bonus.join(' ')
   infoElement = document.createElement('div');
   infoElement.className = 'description-container';
-  infoElement.innerHTML = `<span class="description">${description}</span><span class="price">${price}</span><span class="bonus">${bonus}</span>`;
+  infoElement.innerHTML = `<span class="description">${description}</span><span class="price">&euro;${price}</span><span class="bonus">${bonus}</span>`;
   gameElement.appendChild(infoElement);
 };
 
-function addStars({ userNum, stars }) {
+function addStars({ userNum }, countStars) {
   raitingElement = document.createElement('div');
   raitingElement.className = 'rating-stars';
   raitingElement.innerHTML = `<span>Rating(${userNum})</span>`;
-  container =  document.createElement('div');
-  star = document.createElement('img');
-  star.src = 'assets/star.png';
-  star.alt = 'star';
-  starNum = stars;
-  // for (let i = 1; i <= starNum; i++){
-  //   container.insertAdjacentHTML('afterbegin', star);
-  //   container.classList.add('fill');
-  //   container.appendChild(raitingElement);
-  // }
+  container = document.createElement('div');
+  container.className = 'stars';
+  star = '<img src="assets/star.png" alt="star">';
+  halfStar = '<img src="assets/half-star.png" alt="half-star">';
+  noStar = '<img src="assets/no-star.png" alt="no-star">';
+  starNum = countStars;
+
+  for (let i = 1; i <= starNum; i++){
+    container.insertAdjacentHTML('beforeend', star);
+    container.lastElementChild.classList.add('star');
+    raitingElement.appendChild(container);
+  }
+
+  if(starNum % 1 !== 0) {
+    container.insertAdjacentHTML('beforeend', halfStar);
+    container.lastElementChild.classList.add('star');
+  }
+  if(starNum < 5) {
+    for (let i = 1; i <= 5 - starNum; i++){
+      container.insertAdjacentHTML('beforeend', noStar);
+      container.lastElementChild.classList.add('star');
+    }
+  }
+
   gameElement.appendChild(raitingElement);
 };
 
@@ -122,6 +140,6 @@ function addGameRating({ rating }) {
 function addBtn() {
   btnElement = document.createElement('div');
   btnElement.className = 'btn-container';
-  btnElement.innerHTML = '<button>PLAY</button>';
+  btnElement.innerHTML = '<button class="btn">Play</button>';
   gameElement.appendChild(btnElement);
 };
